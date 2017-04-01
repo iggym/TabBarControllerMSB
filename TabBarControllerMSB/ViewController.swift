84560bfeb2005053967df60f5d86506223f8bc56
iggym <iggy.mw@gmail.com>
2017-04-01 00:29:13 -0500

    Initial Commit

---
+++ TabBarControllerMSB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBarControllerMSB
+//
+//  Created by Iggy Mwangi on 4/1/17.
+//  Copyright © 2017 Iggy Mwangi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
