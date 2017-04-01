f80d00b9884d8aa1cc4e85fe961902d88e0096ac
iggym <iggy.mw@gmail.com>
2017-04-01 02:21:23 -0500

    updates

--- TabBarControllerMSB/GoogleNewsVC.swift
+++ TabBarControllerMSB/GoogleNewsVC.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import UIKit
+
+class GoogleNewsVC : UIViewController {
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        loadWebView(webView: webView, webAddress: "https://news.google.com", label: label)
+    }
+    
+    
+}
