c010d7b9e283da8bbfcf8e26374bfa3398318b6b
iggym <iggy.mw@gmail.com>
2017-04-01 02:19:36 -0500

    updates and changes

--- TabBarControllerMSB/GithubVC.swift
+++ TabBarControllerMSB/GithubVC.swift
@@ -1,23 +1,22 @@
 //
-//  GoogleVC.swift
+//  GithubVC.swift
 //  TabBarControllerMSB
 //
-//  Created by Iggy  on 4/1/17.
+//  Created by Iggy Mwangi on 4/1/17.
 //  Copyright © 2017 Iggy Mwangi. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
-class GoogleVC : UIViewController {
-    
+class GithubVC : UIViewController {
     @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        loadWebView(webView: webView, webAddress: "https://www.google.com", label: label)
+                loadWebView(webView: webView, webAddress: "https://www.github.com", label: label)
     }
-
+    
     
 }
--- TabBarControllerMSB/YouTubeVC.swift
+++ TabBarControllerMSB/YouTubeVC.swift
@@ -1,23 +1,22 @@
 //
-//  GoogleVC.swift
+//  YouTubeVC.swift
 //  TabBarControllerMSB
 //
-//  Created by Iggy  on 4/1/17.
+//  Created by Iggy Mwangi on 4/1/17.
 //  Copyright © 2017 Iggy Mwangi. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
-class GoogleVC : UIViewController {
-    
+class YouTubeVC : UIViewController {
     @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        loadWebView(webView: webView, webAddress: "https://www.google.com", label: label)
+                loadWebView(webView: webView, webAddress: "https://www.youtube.com", label: label)
     }
-
+    
     
 }
