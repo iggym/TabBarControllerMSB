7e18b44b439849d9c2ad1e91174e787d9cfbc1e5
iggym <iggy.mw@gmail.com>
2017-04-01 01:30:33 -0500

    added storyboards , vc's and ui

--- TabBarControllerMSB/AppDelegate.swift
+++ TabBarControllerMSB/AppDelegate.swift
@@ -12,35 +12,48 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let arrayOfImages = [ "1", "2", "3", "4", "5", "6", "7"]
+    let arrayOfTitles = [ "Google", "Bing", "YouTube", "Apple", "Github", "Wikipedia", "GoogleNews"]
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let tabBar = UITabBarController()
+        tabBar.viewControllers = self.getArrayOfViewControllers()
+        self.customiseTabItems(tabBar: tabBar.tabBar)
+        // set tabBar as root
+        
+        self.window?.rootViewController = tabBar
+        self.window?.makeKeyAndVisible()
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    func getArrayOfViewControllers() -> [ViewController] {
+        
+        let googleVC = UIStoryboard.init(name: "Google", bundle: Bundle.main).instantiateInitialViewController()
+        let bingVC = UIStoryboard.init(name: "Bing", bundle: Bundle.main).instantiateInitialViewController()
+        let gooogleNewsVC = UIStoryboard.init(name: "GoogleNews", bundle: Bundle.main).instantiateInitialViewController()
+        let youtubeVC = UIStoryboard.init(name: "YouTube", bundle: Bundle.main).instantiateInitialViewController()
+        let wikipediaVC = UIStoryboard.init(name: "Wikipedia", bundle: Bundle.main).instantiateInitialViewController()
+        let appleVC = UIStoryboard.init(name: "Apple", bundle: Bundle.main).instantiateInitialViewController()
+        let githubVC = UIStoryboard.init(name: "Github", bundle: Bundle.main).instantiateInitialViewController()
+        
+        let arrayOfVC:[UIViewController] = [googleVC!, bingVC!,gooogleNewsVC!, youtubeVC!,wikipediaVC!, appleVC!, githubVC!]
+        
+        return arrayOfVC as! [ViewController]
+        
+    }
+    func customiseTabItems(tabBar: UITabBar) {
+        for item in tabBar.items! {
+            item.image = UIImage(named: arrayOfImages[tabBar.items!.index(of: item)!])
+            item.title = arrayOfTitles[tabBar.items!.index(of: item)!]
+        }
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
+  
+    
+    
 
 }
 
---
+++ TabBarControllerMSB/GoogleVC.swift
@@ -0,0 +1,9 @@
+//
+//  GoogleVC.swift
+//  TabBarControllerMSB
+//
+//  Created by Iggy Mwangi on 4/1/17.
+//  Copyright © 2017 Iggy Mwangi. All rights reserved.
+//
+
+import Foundation
